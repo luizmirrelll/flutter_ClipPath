47ca0d2b8dc80308d2c5b30a151310e3bfe43412
luizmirrelll <aloysius.mirel96@gmail.com>
2020-10-29 16:59:21 +0700

    ClipPath

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("latihan ClipPath"),
+        ),
+        body: Center(
+          child: ClipPath(
+            clipper: MyClipper(),
+            child: Image(
+              width: 300,
+              image: NetworkImage("https://wallpapercave.com/wp/wp1829068.jpg"),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(0, size.height);
+    path.quadraticBezierTo(
+        size.width / 2, size.height * 0.75, size.width, size.height);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
+}
